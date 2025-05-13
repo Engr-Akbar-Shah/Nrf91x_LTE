947d27aa7e6a695493cc7d1b10d0e20dd17bf62f
Engr-Akbar-Shah <engineer.akbarshah@gmail.com>
2025-05-13 13:23:02 +0500

    Added components folder

---
+++ components/lte/lte.h
@@ -0,0 +1,6 @@
+#ifndef _LTE_H
+#define _LTE_H
+
+int modem_configure(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,71 +1,10 @@
-/*
- * Copyright (c) 2020 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
- */
 
 #include <stdio.h>
-#include <ncs_version.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <modem/nrf_modem_lib.h>
-#include <modem/lte_lc.h>
+#include "lte.h"
 
-static K_SEM_DEFINE(lte_connected, 0, 1);
-
-LOG_MODULE_REGISTER(LTE_Nrf91);
-
-static void lte_handler(const struct lte_lc_evt *const evt)
-{
-	switch (evt->type) {
-	case LTE_LC_EVT_NW_REG_STATUS:
-		if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
-			(evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
-			break;
-		}
-		LOG_INF("Network registration status: %s",
-				evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
-				"Connected - home network" : "Connected - roaming");
-		k_sem_give(&lte_connected);
-
-		break;
-	case LTE_LC_EVT_RRC_UPDATE:
-		LOG_INF("RRC mode: %s", evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ?
-				"Connected" : "Idle");
-		break;
-	default:
-		break;
-	}
-}
-
-static int modem_configure(void)
-{
-	int err;
-
-	LOG_INF("Initializing modem library");
-	err = nrf_modem_lib_init();
-	if (err) {
-		LOG_ERR("Failed to initialize the modem library, error: %d", err);
-		return err;
-	}
-
-	/* lte_lc_init deprecated in >= v2.6.0 */
-	#if NCS_VERSION_NUMBER < 0x20600
-	err = lte_lc_init();
-	if (err) {
-		LOG_ERR("Failed to initialize LTE link control library, error: %d", err);
-		return err;
-	}
-	#endif
-	
-	LOG_INF("Connecting to LTE network");
-	err = lte_lc_connect_async(lte_handler);
-	if (err) {
-		LOG_ERR("Error in lte_lc_connect_async, error: %d", err);
-		return err;
-	}
-	return 0;
-}
+LOG_MODULE_REGISTER(Main);
 
 int main(void)
 {
@@ -77,7 +16,7 @@ int main(void)
 		return 0;
 	}
 
-	k_sem_take(&lte_connected, K_FOREVER);
+	// k_sem_take(&lte_connected, K_FOREVER);
 
 	LOG_INF("Connected to LTE network");
 	
